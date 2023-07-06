9ef27c9d71efce769cb4355d60fc37d18e5bb0f9
JulieMille <millesunflower@mail.ru>
2023-07-06 21:13:08 +0300

    user page likes working!

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -1,49 +1,50 @@
 import { USER_POSTS_PAGE, POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
 import { posts, goToPage, getToken, renderApp } from "../index.js";
-import { getPosts, sendLike, sendDislike, posssts } from "../api.js";
+import { getPosts, sendLike, sendDislike, userPosssts } from "../api.js";
+import { renderUserPostsPageComponent } from "./user-posts-page-component.js";
+
+export function initLikes(arr, rendering) {
+  for (let postLike of document.querySelectorAll(".like-button")) {
+    let index = postLike.dataset.index;
+    postLike.addEventListener('click', () => {
+      console.log(`liked already ${postLike.dataset.isliked}`);
+      if (postLike.dataset.isliked === 'true') {
+        sendDislike({
+          token: getToken(),
+          id: postLike.dataset.postId,
+        })
+          .then(() => {
+            getPosts({ getToken });
+            arr[index].isLiked = false;
+            arr[index].likes.forEach((user, indx) => {
+              if (user.id === JSON.parse(window.localStorage.getItem("user"))._id) {
+                arr[index].likes.splice(indx, 1);
+              }
+            })
+          })
+          .then(() => {
+            return rendering();
+          });
+      } else if (postLike.dataset.isliked === 'false') {
+        sendLike({
+          token: getToken(),
+          id: postLike.dataset.postId,
+        })
+          .then(() => {
+            getPosts({ getToken });
+            arr[index].isLiked = true;
+            arr[index].likes.push({ id: JSON.parse(window.localStorage.getItem("user"))._id, name: JSON.parse(window.localStorage.getItem("user")).name });
+          })
+          .then(() => {
+            return rendering();
+          });
+      }
+    });
+  }
+}
 
 export function renderPostsPageComponent({ appEl, posssts }) {
-
-  function initLikes() {
-    for (let postLike of document.querySelectorAll(".like-button")) {
-      let index = postLike.dataset.index;
-      postLike.addEventListener('click', () => {
-        console.log(`liked already ${postLike.dataset.isliked}`);
-        if (postLike.dataset.isliked === 'true') {
-          sendDislike({
-            token: getToken(),
-            id: postLike.dataset.postId,
-          })
-            .then(() => {
-              getPosts({ getToken });
-              posssts[index].isLiked = false;
-              posssts[index].likes.forEach((user, indx) => {
-                if (user.id === JSON.parse(window.localStorage.getItem("user"))._id) {
-                  posssts[index].likes.splice(indx, 1);
-                }
-              })
-            })
-            .then(() => {
-              return rendering();
-            });
-        } else if (postLike.dataset.isliked === 'false') {
-          sendLike({
-            token: getToken(),
-            id: postLike.dataset.postId,
-          })
-            .then(() => {
-              getPosts({ getToken });
-              posssts[index].isLiked = true;
-              posssts[index].likes.push({ id: JSON.parse(window.localStorage.getItem("user"))._id, name: JSON.parse(window.localStorage.getItem("user")).name });
-            })
-            .then(() => {
-              return rendering();
-            });
-        }
-      });
-    }
-  }
 
   function rendering() {
 
@@ -57,7 +58,7 @@ export function renderPostsPageComponent({ appEl, posssts }) {
                           <img class="post-image" src=${possst.imageUrl}>
                         </div>
                         <div class="post-likes">
-                          <button data-post-id=${possst.id} data-userId=${possst.userId} data-username=${possst.userName} data-index=${index} data-isLiked=${possst.isLiked} class="like-button">
+                          <button data-post-id=${possst.id} data-index=${index} data-isLiked=${possst.isLiked} class="like-button">
                             <img src="./assets/images/${possst.isLiked ? "like-active" : "like-not-active"}.svg">
                           </button>
                           <p class="post-likes-text">
@@ -86,8 +87,16 @@ export function renderPostsPageComponent({ appEl, posssts }) {
     renderHeaderComponent({
       element: document.querySelector(".header-container"),
     });
-    initLikes();
+    initLikes(posssts, rendering);
 
+    for (let userEl of document.querySelectorAll(".post-header")) {
+      userEl.addEventListener("click", () => {
+        renderUserPostsPageComponent({appEl, userPosssts});
+        goToPage(USER_POSTS_PAGE, {
+          userId: userEl.dataset.userId,
+        });
+      });
+    }
   };
 
   rendering();
